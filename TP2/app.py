2186fdef8f28568c638145e63f93100fc86622c5
JoaoGome <jprodriguesgomespoe@gmail.com>
2021-06-11 17:47:13 +0100

    json

--- TP2/app.py
+++ TP2/app.py
@@ -1,17 +1,21 @@
 import xmlParser
 import sys
 import os
+import json
 
 from flask import Flask, render_template, request, redirect
 UPLOAD_FOLDER = './files/'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-if (len(sys.argv) < 2):
-    print("Não passou ficheiro xml de config!")
+# exemplo: python3 app.py inputTemplate.xml -csv ou python3 app.py inputTemplate.xml -json
+if (len(sys.argv) < 3):
+    print("Não passou ficheiro xml de config ou formato do ficheiro de resposta")
     exit()
 
 info = xmlParser.parseFile(sys.argv[1])
+results = sys.argv[2][1:]
+print(results)
 id = info["id"]
 info.pop("id")
 
@@ -43,34 +47,51 @@ def base():
 
 @app.route('/upload', methods = ['POST'])
 def upload():
+    
     dic = {}
     first = True
     for x in info.values():
         if x["type"] == "file":
             f = request.files[x["text"]]
             f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
-            dic[x["text"]] = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+            dic[x["text"]] = f.filename
     
         else:
             dic[x["text"]] = request.form[x["text"]]
 
-    if (not os.path.isfile('./results.csv')):
-        with open('results.csv','w') as f:
-            headers = ""
-            for x in dic.keys():
-                headers += f'''{x},'''
-            f.write(headers[:-1]+'\n')
+    if (results == "csv"):
+        if (not os.path.isfile('./results.csv')):
+            with open('results.csv','w') as f:
+                headers = ""
+                for x in dic.keys():
+                    headers += f'''{x},'''
+                f.write(headers[:-1]+'\n')
 
-    with open('results.csv','a') as f:
-        for x in dic.values():
-            if first:
-                f.write(f'''{x}''')
-                first = False 
-            else:
-                f.write(',')
-                f.write(f'''{x}''')
+        with open('results.csv','a') as f:
+            for x in dic.values():
+                if first:
+                    f.write(f'''{x}''')
+                    first = False 
+                else:
+                    f.write(',')
+                    f.write(f'''{x}''')
+            
+            f.write('\n')
+
+    elif (results == "json"):
+        data = []
+        if (not os.path.isfile('./results.json')):
+            with open('results.json','w') as f:
+                f.write('[]')
         
-        f.write('\n')
+        with open('results.json','r') as f:
+            data = json.load(f)
+
+        data.append(dic)
+
+        with open('results.json','w') as f:
+            f.write(json.dumps(data,indent=4))
+
     return redirect("/")
 
 if __name__ == '__main__':
